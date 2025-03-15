5f2b426312bf77f41a37b419789e25f36afb5f2e
SreehariRS <sreeharikannan373@gmail.com>
2025-03-15 14:07:45 +0530

    backend  profile last update

--- services/userApi.ts
+++ services/userApi.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Base API URL
-// const BASE_URL = "https://api.rentalmall.site"
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const BASE_URL = "https://api.rentalmall.site"
+// const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
 // Enable cookies (if needed for session handling)
 axios.defaults.withCredentials = true;
