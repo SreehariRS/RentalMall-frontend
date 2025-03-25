3b8621655e21fbaa014c0270ed332c1f9781bd39
SreehariRS <sreeharikannan373@gmail.com>
2025-03-25 09:10:58 +0530

    check- in 25-03

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,23 +1,22 @@
-"use client"
+"use client";
 import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
 import Avatar from "../Avatar";
 
-interface DesktopItemProps{
-currentUser:User|null
+interface DesktopItemProps {
+  currentUser: User | null;
 }
 
-
-function DesktopSidebar({currentUser}:DesktopItemProps) {
+function DesktopSidebar({ currentUser }: DesktopItemProps) {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
       <nav className="mt-4 flex flex-col justify-between">
-        <ul role="list" className="flex flex-col item-center space-y-1">
+        <ul role="list" className="flex flex-col items-center space-y-1"> {/* Fixed typo: item-center -> items-center */}
           {routes.map((item) => (
             <DesktopItem
               key={item.label}
@@ -25,19 +24,19 @@ function DesktopSidebar({currentUser}:DesktopItemProps) {
               label={item.label}
               icon={item.icon}
               active={item.active}
-              onClick={item.onclick}
+              onClick={item.onClick} // Corrected to onClick
             />
           ))}
         </ul>
       </nav>
       <nav className="mt-4 flex flex-col justify-between items-center">
-    <div onClick={() => setIsOpen(true)}
-      className="cursor-pointer hover:opacity-75 transition"
-      >
-<Avatar src={currentUser?.image || "/images/profile.png"} />
-</div>
-</nav>
-
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition"
+        >
+          <Avatar src={currentUser?.image || "/images/profile.png"} />
+        </div>
+      </nav>
     </div>
   );
 }
