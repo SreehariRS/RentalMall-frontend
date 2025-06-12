22f33ed51c5fd7011b7101aaeadfd2f3eb4658e7
SreehariRS <sreeharikannan373@gmail.com>
2025-06-12 16:01:39 +0530

    added details

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,43 +1,51 @@
-import { Listing, Reservation, User, CancelledReservation,Conversation, Message } from "@prisma/client";
+import { Listing, Reservation, User, CancelledReservation, Conversation, Message } from "@prisma/client";
 
+export type FullMessageType = Message & {
+  sender: User;
+  seen: User[];
+};
 
-export type FullMessageType= Message &{
-    sender:User,
-    seen:User[]
-}
-export type FullConversationType= Conversation &{
-    users:User[],
-    messages:FullMessageType[],
-}
+export type FullConversationType = Conversation & {
+  users: User[];
+  messages: FullMessageType[];
+};
 
 export type safelisting = Omit<Listing, "createdAt"> & {
-    createdAt: string; // Serialized ISO string
-    offerPrice: number | null;
-    rating?: number | null; 
+  createdAt: string;
+  offerPrice: number | null;
+  rating?: number | null;
 };
 
 export type safeReservations = Omit<Reservation, "createdAt" | "startDate" | "endDate" | "listing"> & {
-    createdAt: string;
-    startDate: string;
-    endDate: string;
-    listing: safelisting;
-    canCancel?: boolean;
-    user: {  // Add user field
-        name: string;
-        email: string;
-    };
-};;
+  createdAt: string;
+  startDate: string;
+  endDate: string;
+  listing: safelisting;
+  canCancel?: boolean;
+  user: {
+    name: string;
+    email: string;
+  };
+};
 
 export type SafeCancelledReservations = Omit<CancelledReservation, "cancelledAt" | "startDate" | "endDate" | "listing"> & {
-    cancelledAt: string;
-    startDate: string;
-    endDate: string;
-    listing: safelisting;
+  cancelledAt: string;
+  startDate: string;
+  endDate: string;
+  listing: safelisting;
 };
 
 export type SafeUser = Omit<User, "createdAt" | "updatedAt" | "emailVerified"> & {
-    createdAt: string;
-    updatedAt: string;
-    emailVerified: string | null;
+  createdAt: string;
+  updatedAt: string;
+  emailVerified: string | null;
 };
 
+export type RevenueData = {
+  totalRevenue: number;
+  revenueByProperty: {
+    listingId: string;
+    title: string;
+    revenue: number;
+  }[];
+};
